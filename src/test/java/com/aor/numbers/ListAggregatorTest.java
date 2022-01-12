08796baf3df275c535f6d1c66e5fd4244dde0863
JozeRamos <up202005460@up.pt>
2022-01-12 13:54:11 +0000

    bug 7263

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -1,15 +1,22 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListAggregatorTest {
+    private List<Integer> list;
+
+    @BeforeEach
+    private void helper(){
+        list = Arrays.asList(1,2,4,2,5);
+    }
+
     @Test
     public void sum() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
 
         ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
@@ -19,7 +26,7 @@ public class ListAggregatorTest {
 
     @Test
     public void max() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
+
 
         ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
@@ -29,7 +36,7 @@ public class ListAggregatorTest {
 
     @Test
     public void min() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
+
 
         ListAggregator aggregator = new ListAggregator();
         int min = aggregator.min(list);
@@ -39,11 +46,25 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int distinct = aggregator.distinct(list);
 
         Assertions.assertEquals(4, distinct);
+    }
+
+    @Test
+    public void max_bug_7263(){
+        ListAggregator aggregator = new ListAggregator();
+        int max = aggregator.max(Arrays.asList(-1,-4,-5));
+
+        Assertions.assertEquals(-1, max);
+    }
+
+    @Test
+    public void distinct_bug_8726(){
+        ListAggregator aggregator = new ListAggregator();
+        int distinct = aggregator.distinct(Arrays.asList(1,2,4,2));
+
+        Assertions.assertEquals(3,distinct);
     }
 }
